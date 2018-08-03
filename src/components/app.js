312e614bfde73640994ec1fd6cff6499b0317d3b
Zach Freedman <zfreedman92@gmail.com>
2018-08-03 17:06:37 -0400

    feat: Began skeleton of <TopBar />
    Also started basic <App /> and <TopBar /> styling

--- src/components/app.js
+++ src/components/app.js
@@ -1,11 +1,14 @@
 import React, { Component } from 'react';
+
+import TopBar from "./topbar";
 import './styles/app.css';
+
 
 class App extends Component {
   render() {
     return (
       <div className="app">
-        Coin
+        <TopBar />
       </div>
     );
   }
---
+++ src/components/topbar.js
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+import "./styles/topbar.css";
+
+let TopBar = () => {
+  return (
+    <div className="topbar">
+      Coin
+    </div>
+  );
+}
+
+export default TopBar;
